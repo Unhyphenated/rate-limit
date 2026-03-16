57a4ec032f9ac0aa35e1c707813737b2221071d9
Julian Jong <julian.jong74@gmail.com>
2026-03-16 23:55:18 +0800

    feat: add redis fields

--- internal/models/bucket.go
+++ internal/models/bucket.go
@@ -5,7 +5,6 @@ const RATE = 1
 const MAX_TOKENS = 100
 
 type Bucket struct {
-	Tokens int64
-	LastRefill int64
+    Tokens     int64 `redis:"tokens"`
+    LastRefill int64 `redis:"last_refill"`
 }
-
